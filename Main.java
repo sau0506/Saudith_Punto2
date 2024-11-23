350e7a8762bc8f86e948750c37cf75ced83fc28e
unknown <saudithurangomorales@gmail.com>
2024-11-22 19:42:25 -0500

    cambios

--- Main.java
+++ Main.java
@@ -19,6 +19,7 @@ public class Main {
     
 
         System.out.println("Cadena invertida: " + pilaInvertida);
+        System.out.println("Cantidad de caracteres: "+pilaInvertida.size());
     }
 
    
